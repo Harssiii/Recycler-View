8e74adbb1c0775332d5298f09c4a62d31e383d03
Harssiii <m.harry390@gmail.com>
2022-01-20 00:35:30 -0800

    design changed

--- app/src/main/java/haqnawaz/org/friendsrecyclerview/MainActivity.java
+++ app/src/main/java/haqnawaz/org/friendsrecyclerview/MainActivity.java
@@ -22,18 +22,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Friends f0 = new Friends(1,"Asad", 1980, "Giglgit",R.drawable.d);
-        Friends f1 = new Friends(2,"Zubair", 1981, "Lahore",R.drawable.c);
-        Friends f2 = new Friends(3,"Musa", 1980, "Quetta",R.drawable.b);
-        Friends f3 = new Friends(4,"Nadeem",1987,"Peshawar",R.drawable.a);
-        Friends f4 = new Friends(5,"Shahid", 1980, "Karachi",R.drawable.c);
-        Friends f5 = new Friends(6,"Zia",1987,"Faisalabad ",R.drawable.a);
-        Friends f6 = new Friends(7,"Badar", 1980, "Rawalpindi",R.drawable.d);
-        Friends f7 = new Friends(8,"Hashim",1997,"Karachi",R.drawable.b);
-        Friends f8 = new Friends(8,"Salman",1980,"Quetta",R.drawable.c);
-        Friends f9 = new Friends(8,"Rizwan",1982,"Kasur",R.drawable.d);
-        Friends f10 = new Friends(8,"Junaid",1977,"Islamabad",R.drawable.a);
-        Friends f11 = new Friends(8,"Waseem",1967,"Rawalpindi",R.drawable.b);
+        Friends f0 = new Friends(1,"Home", 0000, "Lahore",R.drawable.d);
+        Friends f1 = new Friends(2,"Cart", 0000, "Lahore",R.drawable.c);
+        Friends f2 = new Friends(3,"Clock", 0000, "Quetta",R.drawable.b);
+        Friends f3 = new Friends(4,"Contacts",0000,"Peshawar",R.drawable.a);
+        Friends f4 = new Friends(5,"Cart", 0000, "Karachi",R.drawable.c);
+        Friends f5 = new Friends(6,"Contact",0000,"Faisalabad ",R.drawable.a);
+        Friends f6 = new Friends(7,"Home", 0000, "Rawalpindi",R.drawable.d);
+        Friends f7 = new Friends(8,"Clock",0000,"Karachi",R.drawable.b);
+        Friends f8 = new Friends(8,"Cary",0000,"Quetta",R.drawable.c);
+        Friends f9 = new Friends(8,"Home",0000,"Kasur",R.drawable.d);
+        Friends f10 = new Friends(8,"Contact",0000,"Islamabad",R.drawable.a);
+        Friends f11 = new Friends(8,"Clock",0000,"Rawalpindi",R.drawable.b);
 
         friendsList.addAll(Arrays.asList(new Friends[]{f0,f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11}));
         recyclerView = findViewById(R.id.myRecyclerView);
